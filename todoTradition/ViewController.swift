ebc7be9d2adb02faa5a0f9956f5489511a651ade
Asif <techieasif@gmail.com>
2021-01-25 10:46:56 +0530

    Initial Commit

---
+++ todoTradition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoTradition
+//
+//  Created by Mohd Asif on 25/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
